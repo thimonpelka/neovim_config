e994b80943d10d0815d88b14b8c4ea9f7c586786
Thimon Pelka <thimon.pelka@gmail.com>
2024-11-16 12:29:37 +0100

    added fugitive

---
+++ lua/plugins/git.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				current_line_blame = true,
+				current_line_blame_opts = {
+					delay = 0,
+				},
+			})
+		end,
+	},
+}
