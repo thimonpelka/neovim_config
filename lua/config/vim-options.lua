76691abf5df5cb9301228a845d151eb7ed973afc
Thimon Pelka <thimon.pelka@gmail.com>
2024-11-19 18:35:29 +0100

    commented out navigation shortcut for quick fix list

--- lua/config/vim-options.lua
+++ lua/config/vim-options.lua
@@ -16,3 +16,8 @@ vim.keymap.set({ "v", "n", "i" }, "<C-d>", "<Esc>Vyp", {})
 
 vim.keymap.set({ "v", "n", "i" }, "<C-i>", "<Esc>[{", {})
 -- vim.keymap.set({ "v", "n", "i" }, "<C-m>", "<Esc>]}", {})
+
+
+-- quickfix menu navigation:
+-- vim.keymap.set("n", "<leader>lj", ":cnext<CR>", { desc="Forward qfixlist"})
+-- vim.keymap.set("n", "<leader>lk", ":cprev<CR>", { desc="Backward qfixlist"})
