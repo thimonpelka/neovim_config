86d696443c930a14afc8a0a2b805d3f64955eda6
Thimon Pelka <thimon.pelka@gmail.com>
2024-11-21 14:36:21 +0100

    changed find_file binding from C-p to <leader>p

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,7 +15,7 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {
+      vim.keymap.set("n", "<leader>p", builtin.find_files, {
 				desc = "Find File",
 				silent = true,
 			})
