b3792fbf0d594e7812b4af66bf0e2e48e90eb327
Thimon Pelka <thimon.pelka@gmail.com>
2024-11-21 21:06:42 +0100

    Commented out vim-fugitive. Trying to only use lazygit from now on

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,24 +1,24 @@
 return {
-	{
-		"tpope/vim-fugitive",
-		config = function()
-			vim.keymap.set({ "n" }, "<leader>ga", ":Git add .<CR>", {
-				desc = "Git add .",
-			})
-			vim.keymap.set({ "n" }, "<leader>gc", ":Git commit -a -m", {
-				desc = "Git commit",
-			})
-			vim.keymap.set({ "n" }, "<leader>gP", ":Git pull<CR>", {
-				desc = "Git pull",
-			})
-			vim.keymap.set({ "n" }, "<leader>gp", ":Git push<CR>", {
-				desc = "Git push",
-			})
-			vim.keymap.set({ "n" }, "<leader>gs", ":Git status<CR>", {
-				desc = "Git status",
-			})
-		end,
-	},
+	-- {
+	-- 	"tpope/vim-fugitive",
+	-- 	config = function()
+	-- 		vim.keymap.set({ "n" }, "<leader>ga", ":Git add .<CR>", {
+	-- 			desc = "Git add .",
+	-- 		})
+	-- 		vim.keymap.set({ "n" }, "<leader>gc", ":Git commit -a -m", {
+	-- 			desc = "Git commit",
+	-- 		})
+	-- 		vim.keymap.set({ "n" }, "<leader>gP", ":Git pull<CR>", {
+	-- 			desc = "Git pull",
+	-- 		})
+	-- 		vim.keymap.set({ "n" }, "<leader>gp", ":Git push<CR>", {
+	-- 			desc = "Git push",
+	-- 		})
+	-- 		vim.keymap.set({ "n" }, "<leader>gs", ":Git status<CR>", {
+	-- 			desc = "Git status",
+	-- 		})
+	-- 	end,
+	-- },
 	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
