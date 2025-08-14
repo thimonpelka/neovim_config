ecd302e6e4021de5d2e0cb5c613a0360007e8344
Thimon Pelka <thimon.pelka@gmail.com>
2025-08-14 12:22:14 +0200

    feat: added roslyn for better csharp dev

---
+++ lua/plugins/roslyn.lua
@@ -0,0 +1,9 @@
+-- Roslyn is necessary for code completion and other features for C# development in Neovim.
+return {
+    "seblyng/roslyn.nvim",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {
+        -- your configuration comes here; leave empty for default settings
+    },
+}
